ff264d67ea4fe23328d81c21c1e269b3f8f6d0f8
Prakhyath07 <prakhz16@gmail.com>
2022-08-15 15:19:42 +0530

    docker tested successully

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ from flask import Flask
 
 app=Flask(__name__)
 
+'''heroku_email='prakhyathb07@gmail.com'
+heroku_api_key='c367f0fe-3ae9-4a91-9506-1820a0a34fcd'
+heroku_app_name='hello-world-prak'
+'''
+# to build image
+'''docker build -t <name in small calse>:<tagname> .'''
+
+# to view image
+'''docker images'''
+
+# to run docker
+""" docker run -p 5000:5000 -e PORT=5000 <dockerid>"""
+
+# to check running containers
+"""docker ps"""
+
+# to stop
+"""docker stop <conatiner id>"""
+
+
+
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return "Starting Machine Learning project"
